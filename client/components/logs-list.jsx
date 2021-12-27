b957fce5049c65107d433ecfca623bcba19dc659
AntonLad <toxa.lada@gmail.com>
2021-12-27 20:43:59 +0300

    change Logs

--- client/components/logs-list.jsx
+++ client/components/logs-list.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
 import { deleteFileOfLogs, logList } from '../redux/reducers/log'
@@ -8,6 +8,11 @@ import Header from './header'
 const Log = ({ history }) => {
   const dispatch = useDispatch()
   const listOfLogs = useSelector((store) => store.log.logs)
+
+  //  useEffect добавлен чтобы при перезагрузке страницы /logs - логи сразу загружались на страницу
+  useEffect(() => {
+    dispatch(logList())
+  }, [dispatch])
   return (
     <div>
       <Header history={history} />
@@ -15,21 +20,30 @@ const Log = ({ history }) => {
         {listOfLogs.map((it) => {
           if (it.action === 'CURRENCY') {
             return (
-              <div key={it.time} className="">{`${it.action} -- change currency to ${it.curency} -- at time: ${it.time}`}</div>
+              <div
+                key={it.time}
+                className=""
+              >{`${it.action} -- change currency to ${it.curency} -- at time: ${it.time}`}</div>
             )
           }
           if (it.action === 'SORT_PRODUCTS_BY_PRICE') {
             return (
-              <div key={it.time}>{`${it.action} -- sort by price ${it.title} -- at time: ${it.time}`}</div>
+              <div
+                key={it.time}
+              >{`${it.action} -- sort by price ${it.title} -- at time: ${it.time}`}</div>
             )
           }
           if (it.action === 'REMOVE_FROM_BASKET') {
             return (
-              <div key={it.time}>{`${it.action} -- remove "${it.title}" from basket -- at time: ${it.time}`}</div>
+              <div
+                key={it.time}
+              >{`${it.action} -- remove "${it.title}" from basket -- at time: ${it.time}`}</div>
             )
           }
           return (
-            <div key={it.time}>{`${it.action} -- added product "${it.title}" to basket -- at time: ${it.time}`}</div>
+            <div
+              key={it.time}
+            >{`${it.action} -- added product "${it.title}" to basket -- at time: ${it.time}`}</div>
           )
         })}
       </div>
@@ -42,7 +56,7 @@ const Log = ({ history }) => {
       >
         Delete logs
       </button>
-      <button
+      {/* <button
         type="button"
         className="bg-red-400 rounded mx-2 py-1 px-1"
         onClick={() => {
@@ -50,7 +64,7 @@ const Log = ({ history }) => {
         }}
       >
         Reload logs
-      </button>
+      </button> */}
     </div>
   )
 }
